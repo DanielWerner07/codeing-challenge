d7fdc93319d55ed56227741baf5148af23ab9b21
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2022-09-04 15:23:10 -0500

    Create valid_parentheses.js

---
+++ Stack/valid_parentheses.js
@@ -0,0 +1,43 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+function isValid(string) {
+    let bracketCount = 0;
+    let curlyBracketCount = 0;
+    let squareBracketCount = 0;
+    for (i in string) {
+        if (string[i] == "(") {
+            bracketCount++;
+        } else if (string[i] == ")") {
+            bracketCount--;
+        }
+        if (string[i] == "{") {
+            curlyBracketCount++;
+        } else if (string[i] == "}") {
+            curlyBracketCount--;
+        }
+        if (string[i] == "[") {
+            squareBracketCount++;
+        } else if (string[i] == "]") {
+            squareBracketCount--;
+        }
+        if (bracketCount < 0 || curlyBracketCount < 0 || squareBracketCount < 0) {
+            return false
+        }
+    }
+    if (bracketCount > 0 || curlyBracketCount > 0 || squareBracketCount > 0) {
+        return false
+    } else {
+        return true
+    }
+}
+
+
+console.log(isValid("()")) //expect true
+console.log(isValid("()[]{}")) //expect true
+console.log(isValid("(]")) //expect false
