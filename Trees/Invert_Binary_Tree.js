4b57d9339e701e947ea4b628c1a03248729c849b
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2022-12-21 16:47:58 -0600

    update tree

---
+++ Trees/Invert_Binary_Tree.js
@@ -0,0 +1,46 @@
+// Given the root of a binary tree, invert the tree, and return its root.
+
+// Example 1:
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+
+// Example 2:
+// Input: root = [2,1,3]
+// Output: [2,3,1]
+
+// Example 3:
+// Input: root = []
+// Output: []
+
+// Constraints:
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+
+function invertTree (root) {
+    let Incress = 2
+    let newRoot = [];
+    let y = 0;
+    let t = 1;
+    newRoot[0] = root[0];
+    for (let i = 1; i < root.length;) {
+        y = (Incress);
+        for (let e = 1; e < (Incress + 1); e++) {
+            console.log(Incress);
+            newRoot[t] = root[y];
+            y--;
+            t++;
+        }
+        console.log("--------------")
+        i += Incress;
+        Incress *= 2;
+    }
+    console.log(root);
+    return newRoot;
+};
+
+root1 = [4,2,7,1,3,6,9];
+root2 = [2,1,3];
+root3 = [];
+console.log(invertTree(root1)); // should be [4,7,2,9,6,3,1]
+// console.log(invertTree(root2)); // should be [2,3,1]
+// console.log(invertTree(root3)); // should be []
