eb1159bdc6847271a2b8966132b6607d6a680101
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2023-06-09 14:05:01 -0500

    Update Two_Numbers_add_to_17.js

--- Two_Numbers_add_to_17.js
+++ Two_Numbers_add_to_17.js
@@ -4,9 +4,18 @@
 
 function addTo17(array) {
     for (let i = 0; i < array.length; i++) {
-        
+        for (let e = (0 + i); e < array.length; e++) {
+            if (array[i] + array[e] == 17) {
+                return true;
+            }
+        }
     }
+    return false;
 }
 
 let array1 = [10, 15, 3, 7];
 console.log(addTo17(array1));
+// should be true
+let array2 = [11, 15, 3, 7];
+console.log(addTo17(array2));
+// should be falses
