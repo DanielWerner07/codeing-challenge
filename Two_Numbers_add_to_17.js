fe66767aca21847b467d4d37363607157dc4ad4b
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2023-06-09 14:00:27 -0500

    Create Two_Numbers_add_to_17.js

---
+++ Two_Numbers_add_to_17.js
@@ -0,0 +1,12 @@
+// Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+// For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+function addTo17(array) {
+    for (let i = 0; i < array.length; i++) {
+        
+    }
+}
+
+let array1 = [10, 15, 3, 7];
+console.log(addTo17(array1));
