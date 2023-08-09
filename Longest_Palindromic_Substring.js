8a9d4a0349e13fcbf83b12972cdc9b21ee26af5c
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2023-08-08 20:34:13 -0500

    Update Longest_Palindromic_Substring.js

--- Longest_Palindromic_Substring.js
+++ Longest_Palindromic_Substring.js
@@ -3,16 +3,14 @@
  
 // substring
 //  in s.
-
  
 
 // Example 1:
-
 // Input: s = "babad"
 // Output: "bab"
 // Explanation: "aba" is also a valid answer.
-// Example 2:
 
+// Example 2:
 // Input: s = "cbbd"
 // Output: "bb"
  
