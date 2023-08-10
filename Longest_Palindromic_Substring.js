b2b67c2306288827ef0114118880f454cc593fc9
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2023-08-09 21:29:18 -0500

    Update Longest_Palindromic_Substring.js

--- Longest_Palindromic_Substring.js
+++ Longest_Palindromic_Substring.js
@@ -1,9 +1,4 @@
-// Given a string s, return the longest 
-// palindromic
- 
-// substring
-//  in s.
- 
+// Given a string s, return the longest palindromic substring in s.
 
 // Example 1:
 // Input: s = "babad"
