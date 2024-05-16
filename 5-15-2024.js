4e807101b6c50310e42909acc6d30cb2215faf94
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2024-05-15 20:30:58 -0500

    Update 5-15-2024.js

--- 5-15-2024.js
+++ 5-15-2024.js
@@ -2,8 +2,30 @@
 
 // For example, for n = 2, one gray code would be [00, 01, 11, 10].
 
-function grayCode(n) {
+function generateGrayCode(n) {
 
-    
+    if (n <= 0) return [];
+
+    let grayCode = ['0', '1'];
+
+    for (let i = 2; i < Math.pow(2, n); i *= 2) {
+        for (let j = i - 1; j >= 0; j--) {
+            grayCode.push(grayCode[j]);
+        }
+
+        for (let j = 0; j < i; j++) {
+            grayCode[j] = '0' + grayCode[j];
+        }
+
+        for (let j = i; j < 2 * i; j++) {
+            grayCode[j] = '1' + grayCode[j];
+        }
+    }
+
+    return grayCode;
 
 }
+
+const n = 3;
+const grayCode = generateGrayCode(n);
+console.log(grayCode);
