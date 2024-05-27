327ba3d9e4ebb42ad73e530b096b5ced7031938c
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2024-05-26 19:57:29 -0500

    Update 5-26-2024.js

--- 5-26-2024.js
+++ 5-26-2024.js
@@ -2,3 +2,18 @@
 
 // For example, given the string "acbbac", return "b". Given the string "abcdef", return null.
 
+function firstRecurringCharacter(str) {
+    const seenChars = new Set();
+    
+    for (let char of str) {
+        if (seenChars.has(char)) {
+            return char;
+        }
+        seenChars.add(char);
+    }
+    
+    return null;
+}
+
+console.log(firstRecurringCharacter("acbbac")); // Output: "b"
+console.log(firstRecurringCharacter("abcdef")); // Output: null
