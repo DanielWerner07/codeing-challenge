b0cb533d3f8ad04f03c5a645941272868638a528
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2024-09-26 14:18:46 -0500

    Update 9-26-2024.js

--- 9-26-2024.js
+++ 9-26-2024.js
@@ -2,3 +2,7 @@
 // we can say that a regular number is one whose only prime divisors are 2, 3, and 5.
 // These numbers have had many applications, from helping ancient Babylonians keep time to tuning instruments according to the diatonic scale.
 // Given an integer N, write a program that returns, in order, the first N regular numbers.
+
+function isRegularNumber(N) {
+        
+}
