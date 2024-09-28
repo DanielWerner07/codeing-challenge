17c7511df0189b7e500a64979eedca5514fa6dd2
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2024-09-28 16:31:57 -0500

    Update 9-28-2024.js

--- 9-28-2024.js
+++ 9-28-2024.js
@@ -10,3 +10,14 @@
 
 // Then a reasonable similarity metric would most likely conclude that a and e are the most similar, so your program should return [('a', 'e')].
 
+const websiteUserPairs = [
+    ['a', 1], ['a', 3], ['a', 5],
+    ['b', 2], ['b', 6],
+    ['c', 1], ['c', 2], ['c', 3], ['c', 4], ['c', 5],
+    ['d', 4], ['d', 5], ['d', 6], ['d', 7],
+    ['e', 1], ['e', 3], ['e', 5], ['e', 6]
+  ];
+
+function similarSites(k) {
+    
+}
